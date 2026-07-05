b608043cc9a14bcfa776937de9c294fabd965d76
aksh <akshusrathe007@gmail.com>
2026-07-05 22:43:36 +0530

    Add Student.java from E:\advance_java_program\Servlet\16_dynamic_response\WEB-INF\src\controllers"

---
+++ 16_dynamic_response/WEB-INF/src/controllers/Student.java
@@ -0,0 +1,13 @@
+package controllers;
+
+public class Student {
+   public String name;
+   public  int age ;
+   public String email;
+
+    Student(String name, int age, String email){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+}
