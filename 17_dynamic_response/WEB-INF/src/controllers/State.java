9660696f06bd15eb204c7993f467f927a98b62a7
aksh <akshusrathe007@gmail.com>
2026-07-05 22:44:03 +0530

    Add State.java from E:\advance_java_program\Servlet\17_dynamic_response\WEB-INF\src\controllers"

---
+++ 17_dynamic_response/WEB-INF/src/controllers/State.java
@@ -0,0 +1,12 @@
+package controllers;
+
+public class State {
+    public int stateid;
+    public String Statename;
+
+
+    State(int stateid, String Statename){
+        this.stateid = stateid;
+        this.Statename = Statename;
+    }
+}
