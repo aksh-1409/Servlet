889b668580c1df747f35a42d5c028be11606373c
aksh <akshusrathe007@gmail.com>
2026-07-05 22:57:24 +0530

    Add FetchUserServlet.java from E:\advance_java_program\Servlet\49_ajax\WEB-INF\src\controllers"

---
+++ 49_ajax/WEB-INF/src/controllers/FetchUserServlet.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import models.User;
+import javax.servlet.http.HttpSession;
+import com.google.gson.Gson;
+import models.User;
+
+@WebServlet("/fetch_user.do")
+public class FetchUserServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        HttpSession session = request.getSession();
+
+        User user = (User)session.getAttribute("user");
+
+        String json = "false";
+        if(user!=null){
+            json = new Gson().toJson(user);
+        }
+    
+
+    response.getWriter().write(json);    
+    } 
+}
+
