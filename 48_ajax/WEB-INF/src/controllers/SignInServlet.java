1b7d05432ab9d517994f8246f852ecd5b452dcea
aksh <akshusrathe007@gmail.com>
2026-07-05 22:56:33 +0530

    Add SignInServlet.java from E:\advance_java_program\Servlet\48_ajax\WEB-INF\src\controllers"

---
+++ 48_ajax/WEB-INF/src/controllers/SignInServlet.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import models.User;
+import javax.servlet.http.HttpSession;
+import com.google.gson.Gson;
+
+@WebServlet("/signin.do")
+public class SignInServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+    HttpSession session = request.getSession();
+    String email =  request.getParameter("email");
+    String password =  request.getParameter("password");
+
+
+  User user = new User(email,password);
+      
+  boolean flag = user.signInUser();
+    if(flag){
+    user.setUserPassword("");
+    session.setAttribute("user",user);
+
+    Gson gson = new Gson();
+    String jsonResp = gson.toJson(user);
+     response.getWriter().print(jsonResp);
+    }else{
+      response.getWriter().print(flag);
+    }
+ 
+  }
+}
