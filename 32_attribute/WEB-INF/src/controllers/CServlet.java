8e4b0e3550f0b16330ce68764bcf18521bd692ad
aksh <akshusrathe007@gmail.com>
2026-07-05 22:49:10 +0530

    Add CServlet.java from E:\advance_java_program\Servlet\32_attribute\WEB-INF\src\controllers"

---
+++ 32_attribute/WEB-INF/src/controllers/CServlet.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.util.Date;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/c.do")
+public class CServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException , ServletException{
+       
+       HttpSession session = request.getSession();
+
+       System.out.println("Session");
+       System.out.println("Id:" + session.getId());
+       System.out.println("Creation Time" + new Date(session.getCreationTime()));
+       System.out.println("Last Accessed Time Time" + new Date(session.getLastAccessedTime()));
+       System.out.println("Is New" + session.isNew());
+       System.out.println("Max Inactive Interval Time" + session.getMaxInactiveInterval());
+
+
+        System.out.println("--------------------------------------------");
+        Integer info = (Integer)session.getAttribute("info");
+        Integer moreInfo = (Integer)session.getAttribute("more_info");
+        System.out.println(info);
+        System.out.println(moreInfo);
+
+       request.getRequestDispatcher("c.html").forward(request,response);
+    }
+}
+
+
