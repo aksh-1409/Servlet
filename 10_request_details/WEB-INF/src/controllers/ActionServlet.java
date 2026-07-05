537fa89355c72f6cde050fe48a52ee515582eede
aksh <akshusrathe007@gmail.com>
2026-07-05 22:42:22 +0530

    Add ActionServlet.java from E:\advance_java_program\Servlet\10_request_details\WEB-INF\src\controllers"

---
+++ 10_request_details/WEB-INF/src/controllers/ActionServlet.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class ActionServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException , ServletException{
+        String localAddr = request.getLocalAddr();
+        String remoteAddr = request.getRemoteAddr();
+        String protocol = request.getProtocol();
+        int remotePort = request.getLocalPort();
+        int localPort = request.getLocalPort();
+        int serverPort = request.getServerPort();
+        String serverName = request.getServerName();
+        boolean isSecure = request.isSecure();
+        String scheme = request.getScheme();
+
+        System.out.println("Local Address: " + localAddr);
+        System.out.println("Remote Address: " + remoteAddr);
+        System.out.println("Protocol: " + protocol);
+        System.out.println("Remote Port: " + remotePort);
+        System.out.println("localPort: " + localPort);
+        System.out.println("serverPort: " + serverPort);
+        System.out.println("serverName: " + serverName);
+        System.out.println("isSecure: " + isSecure);
+        System.out.println("Scheme: " + scheme);
+    }
+}
