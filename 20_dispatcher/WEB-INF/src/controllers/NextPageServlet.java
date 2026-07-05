1514a4b987867d230f67e594475fdcceb09d1a34
aksh <akshusrathe007@gmail.com>
2026-07-05 22:45:36 +0530

    Add NextPageServlet.java from E:\advance_java_program\Servlet\20_dispatcher\WEB-INF\src\controllers"

---
+++ 20_dispatcher/WEB-INF/src/controllers/NextPageServlet.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
+
+@WebServlet("/next.do")
+public class NextPageServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException , ServletException{
+       
+        //request handling ...
+        System.out.println("Hello Next Page Walon...");
+
+
+        //response generation(page creation)
+       RequestDispatcher view = request.getRequestDispatcher("next.html");
+       view.forward(request, response);
+    }
+}
+
+
